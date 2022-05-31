a121703f07aed956cef68f569553f75c4875872c
Hanaglitched <neincredits@knou.ac.kr>
2022-05-31 21:16:52 +0900

    Initial commit
    initial commit of magicnotify

---
+++ src/main/java/com/example/magicnotify/MagicnotifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.magicnotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicnotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicnotifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/magicnotify/MagicnotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.magicnotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicnotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
